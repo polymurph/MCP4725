5af0d5380cd6ae9ac4e77965ce9d6f4a870da302
polymurph <eddyed.k@gmail.com>
2020-06-04 20:35:11 +0200

    work on lib

--- MCP4725cpp/i2c.h
+++ MCP4725cpp/i2c.h
@@ -2,9 +2,12 @@
 #define _I2C_H_
 #include "i_i2c.h"
 
+#include <stdint.h>
+
 class I2C : public I_I2C
 {
     public:
+    I2C();
     void write(uint8_t address, uint8_t* data, uint8_t n);
     private:
 
